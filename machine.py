9351267ed5b97c504a6ab52e5207fe8f74dbea6c
Who23 <40632266+Who23@users.noreply.github.com>
2018-07-18 20:28:33 +0530

    Coded machine.py
    Coded the machine.py class for managing rotors and plugboard
    -swap runs the letter through the plugboard
    -addPlugboardConnection adds a plugboard connection
    -returnOutput returns the output of the enigma machine

---
+++ machine.py
@@ -0,0 +1,24 @@
+from rotors import Rotors
+class Machine():
+    def __init__(self):
+        self.rotor = Rotors()
+        self.reference = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+        self.plugboard = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+    def swap(self, letter):
+        return(self.plugboard[self.reference.index(letter)])
+
+    def addPlugboardConnecion(self, firstLetter, secondLetter):
+        self.plugboard[self.reference.index(firstLetter)] = secondLetter
+        self.plugboard[self.reference.index(secondLetter)] = firstLetter
+    
+    def returnOutput(self, letter):
+        return self.swap(self.rotor.output(self.swap(letter)))
+        
+
+if __name__ == "__main__":
+    machine = Machine()
+    machine.addPlugboardConnecion("A", "B")
+    print(machine.swap("A"))
+    print(machine.swap("B"))
+    print(machine.plugboard)
